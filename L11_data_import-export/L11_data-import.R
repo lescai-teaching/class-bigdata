d55cc89229e2a5ae0565d18773396143e0559176
Francesco L <francesco.lescai@unipv.it>
2023-03-12 16:45:08 +0100

    adding data and code for class L11

---
+++ L11_data_import-export/L11_data-import.R
@@ -0,0 +1,81 @@
+### READR functions
+### how to look at a function's options
+
+formals(read_tsv)
+
+names(formals(read_tsv))
+names(formals(read_csv))
+
+### one can compare options of different functions
+
+intersect(names(formals(read_tsv)), names(formals(read_csv)))
+
+identical(names(formals(read_tsv)), names(formals(read_csv)))
+
+## why this is false?
+## let's order the options
+
+identical(
+  names(formals(read_tsv))[order(names(formals(read_tsv)))], 
+  names(formals(read_csv))[order(names(formals(read_csv)))]
+  )
+
+
+
+##################################################
+## READING DATA 
+##################################################
+
+### NIMBUS DATASET 
+
+data = url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L11_data_import-export/L11_dataset_babynames.rds")
+
+## one can read from the web
+nimbus = read_csv(data)
+## or alternatively reaad it locally
+
+
+## what do you observe?
+nimbus
+
+## inspect one single element
+## observe the role of the function pull()
+nimbus %>% pull(ozone) %>% class()
+
+## one can obtain a similar result with the function pluck()
+## observe the difference between the two
+nimbus %>% pluck("ozone") %>% class()
+
+## what is the conclusion of the vector type
+## compare it with visual inspection of the above dataset
+## why is that?
+
+nimbus %>% pluck("ozone") %>% unique()
+
+## observe better
+
+## let's add an option
+
+nimbus = read_csv(data, na = ".")
+
+nimbus
+
+## see what's changed
+nimbus %>% pluck("ozone") %>% class()
+
+## we can manually specify the column types
+
+nimbus = read_csv(data, 
+					na = ".",
+					col_types = list(
+                    ozone = col_double()
+					)
+					)
+
+### let's see the result
+nimbus
+
+## look at all possible col types
+?cols()
+## and choose readr package
+
---
+++ L11_data_import-export/L11_factors.R
@@ -0,0 +1,96 @@
+
+## we can create a factor with the function factor()
+## the argument x represents the data
+## the levels argument defines the attribute
+
+eyes <- factor(x = c("blue", "green", "green"),
+               levels = c("blue", "brown", "green")
+               )
+
+## when we inspect the object
+eyes
+
+## however its internal representation can be visualise with
+unclass(eyes)
+
+
+## import a dataset
+factorData = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L11_data_import-export/L11_dataset_factors.rds"))
+
+## inspect the dataset
+factorData
+
+## we can already see the class in the tibble
+## but let's run this
+factorData %>% pull(base) %>% class()
+
+## we use the function levels() to access a factor's levels
+
+levels(factorData$base)
+
+## by default they are ordered alphabetically
+
+## REORDER levels
+
+ggplot(factorData, aes(x=base, y=counts))+
+  geom_bar(stat = "identity")
+
+## to help visualising trends and relationshipcs, the factors 
+## should follow the order of data
+
+factorData %>%
+  mutate(
+    base = fct_reorder(base, counts)
+  ) %>%
+  ggplot(aes(x=base, y=counts))+
+  geom_bar(stat = "identity")
+
+## or in a decreasing order
+
+factorData %>%
+  mutate(
+    base = fct_reorder(base, counts, .desc = TRUE)
+  ) %>%
+  ggplot(aes(x=base, y=counts))+
+  geom_bar(stat = "identity")
+
+
+## changing levels can be tricky
+## reconding can be done safely with fct_recode()
+## new level = old level
+
+
+factorData %>%
+  mutate(
+    base = fct_recode(base, 
+                      "Adenine" = "A",
+                      "Guanine" = "G",
+                      "Thymine" = "T",
+                      "Cytosine" = "C"
+                      )
+  )
+
+
+### sometimes we need to collapse groups
+
+factorData %>% 
+  mutate(
+    base = fct_collapse(base,
+                        purines = c("A", "G"),
+                        pyrimidines = c("T", "C")
+                        )
+  )
+
+## this allows us to group
+
+factorData %>% 
+  mutate(
+    base = fct_collapse(base,
+                        purines = c("A", "G"),
+                        pyrimidines = c("T", "C")
+    )
+  ) %>% 
+  group_by(base) %>% 
+  summarise(
+    base_cat_count = sum(counts)
+  )
