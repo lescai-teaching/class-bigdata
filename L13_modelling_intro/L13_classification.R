ee3330b94eccdee7832e2a943d44754348707ce4
Francesco L <francesco.lescai@unipv.it>
2023-03-13 17:10:17 +0100

    added draft for L13

---
+++ L13_modelling_intro/L13_classification.R
@@ -0,0 +1,37 @@
+
+heart_disease_data = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L13_modelling_intro/L13_dataset_heart_disease_data.rds"))
+
+
+## first let's split
+
+heart_disease_split = initial_split(heart_disease_data, prop = 0.8)
+
+## then training and testing
+
+heart_disease_training = training(heart_disease_split)
+heart_disease_testing = testing(heart_disease_split)
+
+### now we prepare the model
+
+rf_model <- rand_forest() %>% 
+  set_mode("classification") %>% 
+  set_engine("ranger")
+
+
+heart_disease_fit = rf_model %>% 
+  fit(heart_disease_risk ~ ., data = heart_disease_training)
+
+
+## let's make a prediction and check it
+
+heart_disease_predictions = heart_disease_testing %>% 
+  bind_cols(
+    predict(heart_disease_fit, heart_disease_testing),
+    predict(heart_disease_fit, heart_disease_testing, type = "prob")
+  )
+
+
+## let's quickly check how our predictions go
+table(heart_disease_predictions$heart_disease_risk, heart_disease_predictions$.pred_class)
+
+## we will discuss in the next class how we can improve this
---
+++ L13_modelling_intro/L13_regression.R
@@ -0,0 +1,89 @@
+
+
+library(tidymodels)
+tidymodels_prefer()
+
+## read the data in
+photosynthesis_data = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L13_modelling_intro/L13_dataset_photosynthesis_data.rds"))
+
+
+## split in training and testing
+set.seed(502)
+photosynthesis_split = initial_split(photosynthesis_data, prop = 0.8)
+
+## inspect the results
+photosynthesis_split
+
+
+## create training data
+photosynthesis_training = training(photosynthesis_split)
+
+## create testing data
+photosynthesis_testing = testing(photosynthesis_split)
+
+## reflect on the need to split with strata
+
+### define the mathematical structure
+
+lm_model <-
+  linear_reg() %>% 
+  set_engine("lm")
+
+
+## fit the model, by defining the relationship
+## between the variables
+
+lm_formula_fit <-
+  lm_model %>% 
+  fit(rate ~ light + temperature + co2, data = photosynthesis_training)
+
+
+## now there's several ways to inspect the model
+## simplest is just
+
+lm_formula_fit
+
+## what has been printed is the fit of the model
+## which can also be extracted with
+
+lm_formula_fit %>% extract_fit_engine()
+
+
+## ths function allows to apply further methods to the fitted model
+## such as
+
+## best way to summarise is using a coherent tidymodels function
+
+tidy(lm_formula_fit)
+
+## all variables are clearly associated with the photosynthesis rate
+
+## now we're ready to make a prediction
+
+predicted_photosynthesis_rate = predict(lm_formula_fit, photosynthesis_testing)
+
+## let's inspect this object
+
+predicted_photosynthesis_rate
+
+## we can also add a confidence interval to the prediction
+
+predicted_photosynthesis_rate = predicted_photosynthesis_rate %>% 
+  bind_cols(
+    predict(lm_formula_fit, photosynthesis_testing, type = "pred_int") ## note addition of "type"
+)
+
+
+### now we can verify what our model predicts
+
+photosynthesis_testing %>% 
+  bind_cols(
+    predicted_photosynthesis_rate
+  ) %>% 
+  ggplot(aes(x=rate, y=.pred))+
+  geom_point()+
+  geom_abline(intercept = 0, slope = 1, colour = "blue")
+
+
+### we can observe that the relationship is not exactly linear
+### something we might have guessed in the initial plots
