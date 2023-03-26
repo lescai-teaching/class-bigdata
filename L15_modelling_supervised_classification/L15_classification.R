4408916fa664c34c3a9c55bc2ff55d448a081764
Francesco L <francesco.lescai@unipv.it>
2023-03-26 19:20:07 +0200

    added code for class L15 and datasets

---
+++ L15_modelling_supervised_classification/L15_classification.R
@@ -0,0 +1,160 @@
+
+logreg_variants = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L15_modelling_supervised_classification/L15_dataset_logreg_variants.rds"))
+
+
+library(tidymodels)
+library(tidyverse)
+
+variants_split = initial_split(logreg_variants %>% 
+                                 dplyr::select(-c(individual)) %>% 
+                                 mutate(phenotype = factor(phenotype, levels = c("control", "case"))),
+                               prop = 0.75)
+variants_training = training(variants_split)
+variants_testing = testing(variants_split)
+
+
+logistic_model <- logistic_reg() %>% 
+  set_engine("glm") %>% 
+  set_mode("classification")
+
+variants_recipe <- recipe(phenotype ~ ., data = variants_training) %>% 
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize()
+
+logreg_wf_variants <- workflow() %>% 
+  add_recipe(variants_recipe) %>% 
+  add_model(logistic_model)
+
+
+logreg_variants_fit <- fit(
+  logreg_wf_variants,
+  variants_training
+)
+
+
+tidy(logreg_variants_fit)
+
+## we can convert the estimates to an odds-ratio
+## and also filter nominally significant variants
+
+tidy(logreg_variants_fit, exponentiate = TRUE) %>% 
+  filter(p.value < 0.05) 
+
+
+phenotype_variants_prediction = bind_cols(
+  variants_testing %>% dplyr::select(phenotype),
+  logreg_variants_fit %>% 
+    predict(variants_testing, type = "class"),
+  logreg_variants_fit %>% 
+    predict(variants_testing, type = "prob")
+)
+  
+phenotype_variants_prediction %>% metrics(truth = phenotype, estimate = .pred_class)
+
+precision(phenotype_variants_prediction, truth = phenotype, estimate = .pred_class)
+recall(phenotype_variants_prediction, truth = phenotype, estimate = .pred_class)
+
+###########################################
+## now let's see by selecting the variants
+
+sig_variants = tidy(logreg_variants_fit, exponentiate = TRUE) %>% 
+  filter(p.value < (0.05/100)) %>% ## remember bonferroni correction
+  filter(term != "(Intercept)") %>% 
+  mutate(
+    variant = sub("^(chr.+_.+)_X.+$", "\\1", term)
+  ) %>% 
+  pull(variant) %>% unique()
+
+## the number of nominally significant variants is:
+length(sig_variants)
+
+## we use for the formula only the significant variants
+## have a look at what the paste0 code does
+
+variants_selection_recipe <- 
+  recipe(as.formula(paste0("phenotype", "~", paste0(sig_variants, collapse = " + "))),
+         data = variants_training) %>% 
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize()
+
+
+
+logreg_wf_selected_variants <- workflow() %>% 
+  add_recipe(variants_selection_recipe) %>% 
+  add_model(logistic_model)
+
+
+logreg_selected_variants_fit <- fit(
+  logreg_wf_selected_variants,
+  variants_training
+)
+
+
+phenotype_selected_variants_prediction = bind_cols(
+  variants_testing %>% dplyr::select(phenotype),
+  logreg_selected_variants_fit %>% 
+    predict(variants_testing, type = "class"),
+  logreg_selected_variants_fit %>% 
+    predict(variants_testing, type = "prob")
+)
+
+phenotype_selected_variants_prediction %>% metrics(truth = phenotype, estimate = .pred_class)
+
+## current model
+precision(phenotype_selected_variants_prediction, truth = phenotype, estimate = .pred_class)
+recall(phenotype_selected_variants_prediction, truth = phenotype, estimate = .pred_class)
+
+## previoius model
+precision(phenotype_variants_prediction, truth = phenotype, estimate = .pred_class)
+recall(phenotype_variants_prediction, truth = phenotype, estimate = .pred_class)
+
+## we have modestly improved in precision, not much in recall
+f_meas(phenotype_variants_prediction, truth = phenotype, estimate = .pred_class)
+f_meas(phenotype_selected_variants_prediction, truth = phenotype, estimate = .pred_class)
+## overall a modest improvement in accuracy
+
+
+################################################
+### RANDOM FOREST CLASSIFIER ###################
+################################################
+
+library(tidymodels)
+
+rf_model <- rand_forest() %>% 
+  set_mode("classification") %>% 
+  set_engine("ranger")
+
+predictors <- names(logreg_variants)[!(names(logreg_variants) %in% c("individual", "phenotype"))]
+
+rf_variants_recipe <- recipe(
+  as.formula(
+    paste0("phenotype ~ ", paste0(predictors, collapse = " + "))
+  ),
+  data = variants_training
+) %>% 
+  step_dummy(all_predictors()) %>% 
+  step_normalize()
+
+rf_class_wf <- workflow() %>% 
+  add_model(rf_model) %>% 
+  add_recipe(rf_variants_recipe)
+
+rf_class_fit <- fit(
+  rf_class_wf,
+  variants_training
+)
+
+
+phenotype_rf_prediction = bind_cols(
+  variants_testing %>% dplyr::select(phenotype),
+  rf_class_fit %>% 
+    predict(variants_testing, type = "class"),
+  rf_class_fit %>% 
+    predict(variants_testing, type = "prob")
+)
+
+
+precision(phenotype_rf_prediction, truth = phenotype, estimate = .pred_class)
+recall(phenotype_rf_prediction, truth = phenotype, estimate = .pred_class)
+f_meas(phenotype_rf_prediction, truth = phenotype, estimate = .pred_class)
+
---
+++ L15_modelling_supervised_classification/L15_tuning.R
@@ -0,0 +1,140 @@
+
+################################################
+### TUNING A WORKFLOW ##########################
+################################################
+
+library(tidymodels)
+library(tidyverse)
+
+logreg_variants = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata-2023/main/L15_modelling_supervised_classification/L15_dataset_logreg_variants.rds"))
+
+
+rf_model_tuning <- rand_forest(
+  mtry = tune(),
+  trees = tune(),
+  min_n = tune()
+) %>% 
+  set_mode("classification") %>% 
+  set_engine("ranger")
+
+
+rf_tuning_grid <- grid_regular(mtry(range = c(5L,8L)),
+                          trees(),
+                          min_n(),
+                          levels = 3)
+
+## testing / training / validation
+## pay attention to the concept
+
+set.seed(324)
+variants_new_split = initial_split(logreg_variants %>% 
+                                     dplyr::select(-c(individual)) %>% 
+                                     mutate(phenotype = factor(phenotype, levels = c("control", "case"))),
+                                   prop = 0.75)
+variants_new_training = training(variants_new_split)
+variants_new_testing = testing(variants_new_split)
+
+## now we further split the training into training itself and validation of the step
+
+set.seed(234)
+variants_folds <- vfold_cv(variants_new_training)
+
+
+## the recipe is going to be the same, as it defines the characteristics
+## of the input, relationships of the variables and transformations
+
+predictors <- names(logreg_variants)[!(names(logreg_variants) %in% c("individual", "phenotype"))]
+
+
+rf_variants_recipe <- recipe(
+  as.formula(
+    paste0("phenotype ~ ", paste0(predictors, collapse = " + "))
+  ),
+  data = variants_new_training
+) %>% 
+  step_dummy(all_predictors()) %>% 
+  step_normalize()
+
+## the workflow instead is created according to the above
+
+rf_class_tune_wf <- workflow() %>% 
+  add_model(rf_model_tuning) %>% 
+  add_recipe(rf_variants_recipe)
+
+## the code to run the fitting looks slightly different
+## since we need to perform a grid tuning
+## the following code might take a while
+## in case too long, stop and load results
+
+rf_tuning_results <- 
+  rf_class_tune_wf %>% 
+  tune_grid(
+    resamples = variants_folds,
+    grid = rf_tuning_grid
+  )
+
+saveRDS(rf_tuning_results, file = "L15_dataset_rf_tuning_results.rds")
+
+# rf_tuning_results = readRDS("L15_dataset_rf_tuning_results.rds")
+
+### we can inspect the outcome metrics as usual
+
+rf_tuning_results %>% 
+  collect_metrics()
+
+### we can plot these results
+
+rf_tuning_results %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x=trees, y=mean, color = factor(min_n)))+
+  geom_line(linewidth = 1.5, alpha = 0.6) +
+  geom_point(size = 2) +
+  facet_wrap(~ .metric, scales = "free", nrow = 2) +
+  scale_x_log10(labels = scales::label_number()) +
+  scale_color_viridis_d(option = "plasma", begin = .9, end = 0)
+
+
+rf_tuning_results %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x=trees, y=mean, color = factor(min_n)))+
+  geom_line(linewidth = 1.5, alpha = 0.6) +
+  geom_point(size = 2) +
+  facet_wrap(.metric ~ mtry, ncol = 3) +
+  scale_x_log10(labels = scales::label_number()) +
+  scale_color_viridis_d(option = "plasma", begin = .9, end = 0)
+
+
+### we can also show the best result according to one
+### metrics
+
+rf_tuning_results %>%
+  show_best("accuracy")
+
+
+## and we can select the model in a model object
+
+rf_tuning_best_model = rf_tuning_results %>%
+  select_best("accuracy")
+
+### in order to USE the model for predictions
+### we need to "finalise" the workflow after tuning
+### using the best model we just saved
+
+final_rf_wf <- rf_class_tune_wf %>% 
+  finalize_workflow(rf_tuning_best_model)
+
+## and do a "last" fit on the split data which will automatically
+## run on the test split
+
+final_rf_fit <- final_rf_wf %>% 
+  last_fit(variants_new_split)
+
+## metrics
+
+final_rf_fit %>% 
+  collect_metrics()
+
+final_rf_fit %>%
+  collect_predictions() %>% 
+  roc_curve(phenotype, .pred_control) %>% 
+  autoplot()
