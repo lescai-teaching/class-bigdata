759ab26b1c47c9526b77eafddf182742344b4c7e
Francesco L <francesco.lescai@unipv.it>
2023-05-09 10:33:55 +0200

    fixed issues with random forest

--- L29_nextflow_assembling-workflow/bin/run_random_forest.R
+++ L29_nextflow_assembling-workflow/bin/run_random_forest.R
@@ -11,6 +11,7 @@ output = args[3]
 writeLines("getting data")
 biodegradation_data = readRDS(input)
 
+
 writeLines("splitting data")
 set.seed(367)
 biodegradation_data_split = initial_split(biodegradation_data, prop = 0.75)
@@ -21,34 +22,31 @@ biodegradation_data_testing = testing(biodegradation_data_split)
 writeLines("assembling workflow")
 
 rf_model_tuning <- rand_forest(
-    mtry = tune(),
-    trees = tune(),
-    min_n = tune()
-    ) %>% 
-    set_mode("regression") %>% 
-    set_engine("ranger")
+  mtry = tune(),
+  trees = tune(),
+  min_n = tune()
+) %>% 
+  set_mode("regression") %>% 
+  set_engine("ranger")
 
 
 rf_tuning_grid <- grid_regular(
-	mtry(range = c(5L,8L)),
-    trees(),
-    min_n(),
-    levels = 3)
+  mtry(range = c(5L,8L)),
+  trees(),
+  min_n(),
+  levels = 3)
 
 
 
 biodegradation_recipe <- 
-    recipe(biodegradation_rate ~ ., 
-        data = biodegradation_data_training) %>% 
-    step_normalize(all_predictors())
-biodegradation_recipe <- 
-    recipe(biodegradation_rate ~ ., 
-        data = biodegradation_data_training) %>% 
-    step_normalize(all_predictors())
+  recipe(biodegradation_rate ~ ., 
+         data = biodegradation_data_training) %>% 
+  step_normalize(all_predictors())
+
 
 rf_regression_tune_wf <- workflow() %>% 
-    add_model(rf_model_tuning) %>% 
-    add_recipe(biodegradation_recipe)
+  add_model(rf_model_tuning) %>% 
+  add_recipe(biodegradation_recipe)
 
 
 set.seed(234)
@@ -58,54 +56,55 @@ biodegradation_folds <- vfold_cv(biodegradation_data_training)
 writeLines("loading doParallel library")
 library(doParallel) 
 writeLines("registering parallel without creating the cluster")
-registerDoParallel(cores=cores)
+registerDoParallel(cores=2)
 
 
 writeLines("beging tuning")
 rf_tuning_results <- 
-    rf_regression_tune_wf %>% 
-    tune_grid(
+  rf_regression_tune_wf %>% 
+  tune_grid(
     resamples = biodegradation_folds,
     grid = rf_tuning_grid
-    )
+  )
 
 writeLines("tuning completed - collecting metrics")
 
 rf_tuning_results %>% 
-    collect_metrics() %>%
-    write_tsv(file = paste0(output, "_randomforest_tuning_metrics.tsv"))
+  collect_metrics() %>%
+  write_tsv(file = paste0(output, "_randomforest_tuning_metrics.tsv"))
 
 
 writeLines("finalising workflow")
 
 rf_tuning_best_params = rf_tuning_results %>%
-    select_best("accuracy")
+  select_best("rmse")
 
-final_rf_wf <- rf_class_tune_wf %>% 
-    finalize_workflow(rf_tuning_best_params)
+final_rf_wf <- rf_regression_tune_wf %>% 
+  finalize_workflow(rf_tuning_best_params)
 
 final_rf_fit <- final_rf_wf %>% 
-    last_fit(biodegradation_data_split)
+  last_fit(biodegradation_data_split)
 
 ## metrics
 
 
 final_metrics = final_rf_fit %>% 
-    collect_metrics()
+  collect_metrics()
 write_tsv(final_metrics, file = paste0(output, "_randomforest_final_metrics.tsv"))
 
 writeLines("plotting predictions")
 
-# pdf(paste0(output, "_rf_prediction_plot.pdf"))
-# final_rf_fit %>%
-#     collect_predictions() %>% 
-#     roc_curve(biodegradation_rate, .pred_control) %>% 
-#     autoplot()
-# dev.off()
+pdf(paste0(output, "_rf_prediction_plot.pdf"))
+final_rf_fit %>%
+  collect_predictions() %>%
+  ggplot(aes(x=biodegradation_rate, y=.pred))+
+  geom_point(alpha = 0.4, colour = "blue")+
+  geom_abline(colour = "red", alpha = 0.9)
+dev.off()
 
 rf_tuning_best_model <- finalize_model(
-    rf_model_tuning, ## this is the model we initially created with tune placeholders
-    rf_tuning_best_params ## these are the best parameters identified in tuning
+  rf_model_tuning, ## this is the model we initially created with tune placeholders
+  rf_tuning_best_params ## these are the best parameters identified in tuning
 )
 
 writeLines("plotting importance")
@@ -114,10 +113,10 @@ library(vip)
 
 pdf(paste0(output, "_rf_importance_plot.pdf"))
 rf_tuning_best_model %>%
-    set_engine("ranger", importance = "permutation") %>%
-    fit(
-        biodegradation_rate ~ .,
-        data = variants_new_testing
-    ) %>%
-    vip(geom = "point")
+  set_engine("ranger", importance = "permutation") %>%
+  fit(
+    biodegradation_rate ~ .,
+    data = biodegradation_data_testing
+  ) %>%
+  vip(geom = "point")
 dev.off()
