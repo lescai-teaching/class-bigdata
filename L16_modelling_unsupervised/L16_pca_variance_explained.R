661c0426b59060634cc3b0acf083ee1182394fad
Francesco Lescai <53608000+lescai@users.noreply.github.com>
2026-04-27 21:56:07 +0200

    Update PCA script to use cell culture data
    Removed genotypes data processing and replaced with cell culture data for PCA analysis.

--- L16_modelling_unsupervised/L16_pca_variance_explained.R
+++ L16_modelling_unsupervised/L16_pca_variance_explained.R
@@ -2,44 +2,21 @@ library(tidyverse)
 library(tidymodels)
 
 
-genotypes = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata/main/L12_tidy_eda/L12_dataset_genotypes.rds"))
-
-
-genotyped_individuals = genotypes %>% 
-  pivot_longer(
-    cols = starts_with("sim"),
-    names_to = "individual",
-    values_to = "genotype"
-  ) %>% 
-  mutate(
-    genotype = case_when(
-      genotype == "0/0" ~ 0,
-      genotype == "0/1" ~ 1,
-      genotype == "1/1" ~ 2,
-      TRUE ~ NA
-    )
-  ) %>% 
-  pivot_wider(
-    names_from = "variant",
-    values_from = "genotype"
-  )
-
+dataCellCulture = readRDS(url("https://raw.githubusercontent.com/lescai-teaching/class-bigdata/main/L16_modelling_unsupervised/L16_dataset_cellculture_advanced.rds"))
 
 ### PCA recipe
 
 num_pcs = 10
 
-pca_data = genotyped_individuals %>% 
-  select(-c(individual))
 
-pca_recipe <- recipe(~., data = pca_data) %>% 
+pca_recipe <- recipe(~., data = dataCellCulture) %>% 
   step_normalize(all_numeric()) %>% 
   step_pca(all_numeric(), num_comp = num_pcs)
 
 ## prep() estimates the PCA transformation
 pca_estimates <- prep(
   pca_recipe,
-  training = pca_data
+  training = dataCellCulture
 )
 
 
